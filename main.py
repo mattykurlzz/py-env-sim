fb868e2ea250d0a6997102ee1d9afaa3737a4987
mattykurlzz <mamontovvanya@yandex.ru>
2025-09-29 17:55:06 +0300

    add acceleration

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-coordinates = [0, 0]
+import numpy as np
+
+datatype = np.float32
+current_coordinates = np.array([0, 0], dtype=datatype)
+current_speed = np.array([0, 0], dtype=datatype)
+prev_speed = np.array([0, 0], dtype=datatype)
+current_acceleration = np.array([1, 1], dtype=datatype)
+
 time = 0
 time_step = 1
-
-speeds = [10, 10]
 for _ in range(100):
     time += time_step
-    for i in range(len(coordinates)):
-        coordinates[i] += speeds[i] * time_step
-    print(coordinates)
+    prev_speed = current_speed
+
+    current_speed += current_acceleration * time_step 
+    current_coordinates += ( current_speed * time_step + prev_speed * time_step ) / 2
+
+    print(current_coordinates)
